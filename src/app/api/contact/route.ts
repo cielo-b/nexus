f7b146eb0e0f259c4f4fc23cfe2e6950043dd44b
yves mugisha <yvesmugisha84@gmail.com>
2025-10-03 08:05:57 +0200

    [fix]

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import nodemailer from 'nodemailer'
 import { z } from 'zod'
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 
 
 // Validation schema
